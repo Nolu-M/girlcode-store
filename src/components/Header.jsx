0916b3251963d4cad4e4e3e27c8f98d1894f174f
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-06-09 22:53:48 +0200

    Tailwind styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 export default function() {
     return (
-        <header className="header-base">
-            <div className="header-contact">
-                <span class="material-symbols-outlined">phone</span>
-                <a href="+27 11 546 7899" className="contact-number">+27 11 546 7899</a>
+        <header className='flex justify-between bg-[#e94154] font-primary text-white p-2.5'>
+            <div className='ml-10'>
+                <span class='material-symbols-outlined align-middle'>phone</span>
+                <a href="+27 11 546 7899" className=''>+27 11 546 7899</a>
             </div>
             <div className="header-info">
-                <p>Get 50% off on Selected Items | <a href="#">Shop now</a></p>
+                <p>Get 50% off on Selected Items | <a href="#" className='underline'>Shop now</a></p>
             </div>
-            <div className="header-dropdown">
-                <select name="language" id="language">
+            <div className='mr-10'>
+                <select name="language" id="language" className='bg-[#e94154]'>
                     <option value="english">English</option>
                     <option value="IsiXhosa">Xhosa</option>
                     <option value="IsiZulu">IsiZulu</option>
                 </select>
-                <select name="location" id="location">
+                <select name="location" id="location" className='bg-[#e94154] ml-2.5'>
                     <option value="location">Location</option>
                     <option value="cape-town">Cape Town</option>
                     <option value="johannesburg">Johannesburg</option>
