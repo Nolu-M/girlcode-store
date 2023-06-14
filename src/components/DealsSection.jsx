f6ed274abcb776dd12c740bbcea89c4285e9b7ad
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-06-14 23:34:53 +0200

    Deals section changes

--- src/components/DealsSection.jsx
+++ src/components/DealsSection.jsx
@@ -1,5 +1,6 @@
 import DealsImg from '../assets/deals.png'
 import StarSvg from '../assets/star.svg'
+import HeartSvg from '../assets/heart.svg'
 
 export default function() {
     return (
@@ -8,21 +9,61 @@ export default function() {
                 <div className='mb-9'>
                     <h2 className='text-2xl font-extrabold'>Todays Best Deals For You!</h2>
                 </div>
-                <div className='grid grid-cols-6 gap-8 rounded-xl overflow-hidden bg-[#f5f6f6] text-center mb-7'>
-                    <div className='rounded-xl overflow-hidden'>
-                        <img src={DealsImg} alt="Deals Category"/>
-                        <i></i>
+                <div className='grid grid-cols-3 gap-8'>
+                    <div className='rounded-xl overflow-hidden bg-[#f5f6f6] text-center mb-7'>
+                        <div className='relative'>
+                            <img className=''src={DealsImg} alt="Deals Category"/>
+                            <img className='absolute top-4 right-4 items-center bg-[#fff] w-10 h-10 rounded-full'src={HeartSvg} alt="Heart icon" />
+                        </div>
+                    </div>
+                    <div className='rounded-xl overflow-hidden bg-[#f5f6f6] text-center mb-7'>
+                        <div className='relative'>
+                            <img className=''src={DealsImg} alt="Deals Category"/>
+                            <img className='absolute top-4 right-4 items-center bg-[#fff] w-10 h-10 rounded-full'src={HeartSvg} alt="Heart icon" />
+                        </div>
+                    </div>
+                    <div className='rounded-xl overflow-hidden bg-[#f5f6f6] text-center mb-7'>
+                        <div className='relative'>
+                            <img className=''src={DealsImg} alt="Deals Category"/>
+                            <img className='absolute top-4 right-4 items-center bg-[#fff] w-10 h-10 rounded-full'src={HeartSvg} alt="Heart icon" />
+                        </div>
                     </div>
                 </div>
-                <div className='leading-5'>
+                <div className='flex items-center flex-row'>
+                <h3 className='text-lg font-semibold text-[#231f1e] mt-4 mb-3.5'>HomePod mini</h3>
+                    <p className='mb-2.5'>Table with air purifier, stained veneer/black</p>
+                        <div className='mb-8 flex'>
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=' mr-0.5'src={StarSvg} alt="star rating" />
+                            <p className='ml-1'> (121)</p>
+                        </div>
+                            <a className='bg-[#e94154] text-base text-[#fff] font-semibold py-3.5 px-7 rounded-3xl' href='#'>Add to Cart</a>
                     <h3 className='text-lg font-semibold text-[#231f1e] mt-4 mb-3.5'>HomePod mini</h3>
-                    <p>Table with air purifier, stained veneer/black</p>
-                    <div className='mb-4'>
-                    <img className='inline-block'src={StarSvg} alt="star rating" />
-                    <p className='block'>(121)</p>
+                    <p className='mb-2.5'>Table with air purifier, stained veneer/black</p>
+                        <div className='mb-8 flex'>
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=' mr-0.5'src={StarSvg} alt="star rating" />
+                            <p className='ml-1'> (121)</p>
+                        </div>
+                            <a className='bg-[#e94154] text-base text-[#fff] font-semibold py-3.5 px-7 rounded-3xl' href='#'>Add to Cart</a>
+                            <h3 className='text-lg font-semibold text-[#231f1e] mt-4 mb-3.5'>HomePod mini</h3>
+                    <p className='mb-2.5'>Table with air purifier, stained veneer/black</p>
+                        <div className='mb-8 flex'>
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=''src={StarSvg} alt="star rating" />
+                            <img className=' mr-0.5'src={StarSvg} alt="star rating" />
+                            <p className='ml-1'> (121)</p>
+                        </div>
+                            <a className='bg-[#e94154] text-base text-[#fff] font-semibold py-3.5 px-7 rounded-3xl' href='#'>Add to Cart</a>
                     </div>
-                    <a className='bg-[#e94154] text-base text-[#fff] font-semibold py-3.5 px-7 rounded-3xl' href='#'>Add to Cart</a>
-                </div>
             </div>
         </section>
     )
---
+++ src/components/deals.md
@@ -0,0 +1,28 @@
+<div className='rounded-xl overflow-hidden bg-[#f5f6f6] text-center mb-7'>
+                        <div className='relative'>
+                            <img className=''src={DealsImg} alt="Deals Category"/>
+                            <img className='absolute top-4 right-4 items-center bg-[#fff] w-10 h-10 rounded-full'src={HeartSvg} alt="Heart icon" />
+                        </div>
+                    </div>
+                    <div className='rounded-xl overflow-hidden bg-[#f5f6f6] text-center mb-7'>
+                        <div className='relative'>
+                            <img className=''src={DealsImg} alt="Deals Category"/>
+                            <img className='absolute top-4 right-4 items-center bg-[#fff] w-10 h-10 rounded-full'src={HeartSvg} alt="Heart icon" />
+                        </div>
+                    </div>
+
+
+
+                    <div className='leading-5'>
+                    <h3 className='text-lg font-semibold text-[#231f1e] mt-4 mb-3.5'>HomePod mini</h3>
+                    <p className='mb-2.5'>Table with air purifier, stained veneer/black</p>
+                    <div className='mb-8 flex'>
+                    <img className=''src={StarSvg} alt="star rating" />
+                    <img className=''src={StarSvg} alt="star rating" />
+                    <img className=''src={StarSvg} alt="star rating" />
+                    <img className=''src={StarSvg} alt="star rating" />
+                    <img className=' mr-0.5'src={StarSvg} alt="star rating" />
+                    <p className='ml-1'> (121)</p>
+                    </div>
+                    <a className='bg-[#e94154] text-base text-[#fff] font-semibold py-3.5 px-7 rounded-3xl' href='#'>Add to Cart</a>
+                </div>
