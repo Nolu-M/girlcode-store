f4a550b416c447f521b798813f35a021cfb24a76
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-06-20 08:44:08 +0200

    Deals Routing

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Category/:catName' element={<Category />} />
-        <Route path='/Deals' element={<Deals />} />
+        <Route path='/Deals/: dealsName' element={<Deals />} />
       </Routes>
     </>
     
